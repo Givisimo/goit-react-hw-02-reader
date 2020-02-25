c105518dfc1f232829e39b041cc90992d4e63571
Denys Plietnikov <givisimo@gmail.com>
2020-02-25 20:18:17 +0200

    hooks practise

--- src/Reader/Reader.jsx
+++ src/Reader/Reader.jsx
@@ -1,57 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import T from 'prop-types';
 import Controls from '../Controls/Controls';
 import Progress from '../Progress/Progress';
 import Publication from '../Publication/Publication';
 
-class Reader extends Component {
-  state = {
-    publicationIndex: 0,
+const Reader = ({ items }) => {
+  const [publicationIndex, handleIndex] = useState(0);
+
+  const onIncreaseClick = () => {
+    handleIndex(publicationIndex + 1);
+  };
+  const onDecreaseClick = () => {
+    handleIndex(publicationIndex - 1);
   };
 
-  static propTypes = {
-    items: T.arrayOf(
-      T.shape({
-        id: T.string.isRequired,
-        title: T.string.isRequired,
-        text: T.string.isRequired,
-      }).isRequired,
-    ).isRequired,
-  };
+  const { length } = items;
+  const publication = items[publicationIndex];
+  const { text, title } = publication;
+  const nextBtnDisabled = publicationIndex + 1 === length;
 
-  onIncreaseClick = () => {
-    this.setState(state => ({
-      publicationIndex: state.publicationIndex + 1,
-    }));
-  };
+  return (
+    <>
+      <Controls
+        onNextClick={onIncreaseClick}
+        onPrevClick={onDecreaseClick}
+        prevBtnDisabled={!publicationIndex}
+        nextBtnDisabled={nextBtnDisabled}
+      />
+      <Progress currentPage={publicationIndex} totalPages={length} />
+      <Publication title={title} text={text} />
+    </>
+  );
+};
 
-  onDecreaseClick = () => {
-    this.setState(state => ({
-      publicationIndex: state.publicationIndex - 1,
-    }));
-  };
-
-  render() {
-    const { publicationIndex } = this.state;
-    const { items } = this.props;
-    const { length } = items;
-    const publication = items[publicationIndex];
-    const { text, title } = publication;
-    const nextBtnDisabled = publicationIndex + 1 === length;
-
-    return (
-      <>
-        <Controls
-          onNextClick={this.onIncreaseClick}
-          onPrevClick={this.onDecreaseClick}
-          prevBtnDisabled={!publicationIndex}
-          nextBtnDisabled={nextBtnDisabled}
-        />
-        <Progress currentPage={publicationIndex} totalPages={length} />
-        <Publication title={title} text={text} />
-      </>
-    );
-  }
-}
+Reader.propTypes = {
+  items: T.arrayOf(
+    T.shape({
+      id: T.string.isRequired,
+      title: T.string.isRequired,
+      text: T.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Reader;
